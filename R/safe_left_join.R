e653088f1f47acda608eff20c62dfb2ea85c034c
samedwardes <rstudio@rstudio-quickstart.vm>
2021-04-18 16:56:55 +0000

    Cleaned up safe_left_join and added better docs.

--- R/safe_left_join.R
+++ R/safe_left_join.R
@@ -1,17 +1,60 @@
-#' Safe left join
+#' Validate extra rows are added not added to the left hand side
+#'
+#' @export
+#' @description
+#' Perform a "safe" left join where it is guaranteed that no additional rows are
+#' added to the left hand side table.
 #'
 #' @inheritDotParams dplyr::left_join
 #' @param action What should happen when the number of rows changes from a join?
 #'   Options include: 'error', 'warning', or 'message'. By default 'error'.
+#' @param relationship What is the expected relationship between `x` and `y`?
+#'   At this time the only available option is '*:1', indciating a many to one
+#'   relationship between `x` and `y`. In the future more options may be added.
 #'
 #' @return
-#' @export
+#' #' An object of the same type as `x`. The order of the rows and columns of `x`
+#' is preserved as much as possible. The output has the following properties:
 #'
 #' @examples
+#' # The relationship between `x` and `y` is '*:1'. No extra rows will be added
+#' # to the left hand side.
+#' x <- data.frame(key = c("a", "a", "b"), value_x = c(1, 4, 2))
+#' y <- data.frame(key = c("a", "b"), value_y = c(1, 1))
+#' safe_left_join(x, y)
+#'
+#' # The relationship between `x` and `y` is '1:*'. An error should be raised
+#' # because additional rows will be added to the left hand side.
 #' x <- data.frame(key = c("a", "b"), value_x = c(1, 2))
 #' y <- data.frame(key = c("a", "a"), value_y = c(1, 1))
 #' safe_left_join(x, y)
-safe_left_join <- function(..., action="error") {
+#'
+#' # Alternatively instead of raising an error a warning or message can be
+#' outputed.
+#' safe_left_join(x, y, action = "warning")
+#' safe_left_join(x, y, action = "message")
+safe_left_join <- function(..., action = "error", relationship = "*:1") {
+
+  # Validate parameters
+  action_options <- c("error", "warning", "message")
+
+  if (!(action %in% action_options)) {
+    stop(glue::glue(
+      "`action` must be one of: ",
+      "{glue::glue_collapse(action_options, ', ')}"
+    ))
+  }
+
+  relationship_options <- c("*:1")
+  relationship_future_options <- c("*:1", "1:*", "1:1", "*:*")
+
+  if (!(relationship %in% relationship_options)) {
+    stop(glue::glue(
+      "`action` must be one of: ",
+      "{glue::glue_collapse(relationship_options, ', ')}"
+    ))
+  }
+
   # Parse parameters
   params <- list(...)
   if (is.null(params$x)) {
@@ -20,13 +63,18 @@ safe_left_join <- function(..., action="error") {
     x <- params$x
   }
 
-
   # Perform the join
   data <- dplyr::left_join(...)
 
-  # Test if safe
+  # Create the message to display
+  msg <- glue::glue(
+    "Input data x had {nrow(x)} rows. ",
+    "After performing the join the data has {nrow(data)} rows."
+  )
+
+  # Test if the selected relationship has been respected and raise the
+  # appropriate error, warning, or message
   if (nrow(x) != nrow(data)) {
-    msg <- glue::glue("Input data x had {nrow(x)} rows. After performing the join the data has {nrow(data)} rows.")
     if (action == "message") {
       message(msg)
     } else if (action == "warning") {
@@ -35,6 +83,9 @@ safe_left_join <- function(..., action="error") {
       stop(msg)
     }
   }
+
+    {
+    }
 
   data
 }
