cee04e939e77a67277f0a9ff8df88438e8875699
samedwardes <rstudio@rstudio-quickstart.vm>
2021-04-18 16:57:09 +0000

    Added more tests for safe_left_join

--- tests/testthat/test-safe_left_join.R
+++ tests/testthat/test-safe_left_join.R
@@ -1,34 +1,52 @@
 context("test-safe_left_join")
 
-test_that("Error is thrown when rows are added to x.", {
-  x1 <- dplyr::tibble(
-    key = c(1, 2, 3, 4, 5),
-    value_x = c(33, 44, 12, 55, 33)
-  )
+x <- dplyr::tribble(
+  ~key, ~value_x,
+  "a",  1,
+  "b",  2,
+  "c",  3,
+  "c",  3,
+  "d",  4
+)
 
-  y1 <- dplyr::tibble(
-    key = c(1, 2, 3, 4, 5, 5),
-    value_y = c(33, 44, 12, 55, 33, 55)
-  )
+safe_y <- dplyr::tribble(
+  ~key, ~value_y,
+  "a",  1,
+  "b",  2,
+  "c",  3,
+  "d",  4,
+  "e",  9
+)
 
-  expect_error(safe_left_join(x1, y1))
+dangerous_y <- dplyr::tribble(
+  ~key, ~value_y,
+  "a",  1,
+  "a",  9,
+  "b",  2,
+  "c",  3,
+  "d",  4
+)
+
+test_that("A safe join has the correct number of rows.", {
+  expect_equal(nrow(safe_left_join(x, safe_y)), 5)
 })
 
+test_that("Error is thrown when rows are added to x.", {
+  expect_error(safe_left_join(x, dangerous_y))
+})
 
-test_that("Safe join has the correct number of rows.", {
-  x2 <- dplyr::tibble(
-    key = c(1, 2, 3, 4, 5, 6),
-    value_x = c(33, 44, 12, 55, 33, 66)
-  )
+test_that("Message is thrown when rows are added to x.", {
+  expect_message(safe_left_join(x, dangerous_y, action = "message"))
+})
 
-  y2 <- dplyr::tibble(
-    key = c(1, 2, 3, 4, 5),
-    value_y = c(33, 44, 12, 55, 33)
-  )
+test_that("Warning is thrown when rows are added to x.", {
+  expect_warning(safe_left_join(x, dangerous_y, action = "warning"))
+})
 
-  data <- safe_left_join(x2, y2)
+test_that("A bad action parameter raises an error.", {
+  expect_error(nrow(safe_left_join(x, safe_y, action = "BAD")))
+})
 
-  expect_equal(nrow(data), 6)
-
-
+test_that("A bad relationship parameter raises an error.", {
+  expect_error(nrow(safe_left_join(x, safe_y, relationship = "BAD")))
 })
